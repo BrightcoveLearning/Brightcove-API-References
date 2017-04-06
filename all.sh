fe51118f1f6d25ea06b7f6c907ad1c13532757b0
Robert Crooks <rcrooks@brightcove.com>
2017-04-06 13:36:25 -0400

    Rebuilding all apis

--- all.sh
+++ all.sh
@@ -10,67 +10,67 @@ apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-new
 echo ${txtgrn}finished generating docs
 echo ${txtgrn}Finished!
 echo ${txtrst}
-cd audience-api
+cd ../audience-api
 echo ${txtyel}generating docs for audience-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-new
 echo ${txtgrn}Finished!
 echo ${txtrst}
-cd cms-api
+cd ../cms-api
 echo ${txtyel}generating docs for cms-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-new
 echo ${txtgrn}Finished!
 echo ${txtrst}
-cd data-collection-api
+cd ../data-collection-api
 echo ${txtyel}generating docs for data-collection-api
 apidoc -i v2/src/  -f .js -o v2/doc/ -t ../template-new
 echo ${txtgrn}Finished!
 echo ${txtrst}
-cd dynamic-ingest-api
+cd ../dynamic-ingest-api
 echo ${txtyel}generating docs for dynamic-ingest-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-new
 echo ${txtgrn}Finished!
 echo ${txtrst}
-cd ingest-profiles-api
+cd ../ingest-profiles-api
 echo ${txtyel}generating docs for ingest-profiles-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-new
 echo ${txtgrn}Finished!
 echo ${txtrst}
-cd live-api
+cd ../live-api
 echo ${txtyel}generating docs for live-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-new
 echo ${txtgrn}Finished!
 echo ${txtrst}
-cd oauth-api
+cd ../oauth-api
 echo ${txtyel}generating docs for oauth-api
 apidoc -i v4/src/  -f .js -o v4/doc/ -t ../template-new
 echo ${txtgrn}Finished!
 echo ${txtrst}
-cd once-ingest-api
+cd ../once-ingest-api
 echo ${txtyel}generating docs for once-ingest-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-new
 echo ${txtgrn}Finished!
 echo ${txtrst}
-cd once-media-management-api
+cd ../once-media-management-api
 echo ${txtyel}generating docs for once-media-management-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-new
 echo ${txtgrn}Finished!
 echo ${txtrst}
-cd once-status-api
+cd ../once-status-api
 echo ${txtyel}generating docs for once-status-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-new
 echo ${txtgrn}Finished!
 echo ${txtrst}
-cd playback-api
+cd ../playback-api
 echo ${txtyel}generating docs for playback-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-new
 echo ${txtgrn}Finished!
 echo ${txtrst}
-cd player-management-api
+cd ../player-management-api
 echo ${txtyel}generating docs for player-management-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-new
 echo ${txtgrn}Finished!
 echo ${txtrst}
-cd policy-api
+cd ../policy-api
 echo ${txtyel}generating docs for policy-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-new
 echo ${txtgrn}Finished!
