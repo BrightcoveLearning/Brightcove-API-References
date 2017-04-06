2d2fa9e097cab8c881ff046dc453c03fc2a5dc78
Robert Crooks <rcrooks@brightcove.com>
2017-04-06 12:50:23 -0400

    Updating run scripts

--- dcapi.sh
+++ dcapi.sh
@@ -2,7 +2,6 @@ txtrst=$(tput sgr0) # Text reset
 txtyel=$(tput setaf 7) # Yellow
 txtgrn=$(tput setaf 6) # Green
 txtred=$(tput setaf 9) # red
-echo ${txtred}Generating API docs...
 cd data-collection-api
 echo ${txtyel}generating docs for data-collection-api
 apidoc -i v2/src/  -f .js -o v2/doc/ -t ../template-new
--- pmapi.sh
+++ pmapi.sh
@@ -2,7 +2,6 @@ txtrst=$(tput sgr0) # Text reset
 txtyel=$(tput setaf 7) # Yellow
 txtgrn=$(tput setaf 6) # Green
 txtred=$(tput setaf 9) # red
-echo ${txtred}Generating Player Management API docs...
 cd player-management-api
 echo ${txtyel}generating docs for player-management-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-new
