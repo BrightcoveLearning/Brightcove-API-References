74f942b9c896ec340b96b34851b4e06a2f366caf
Robert Crooks <rcrooks@brightcove.com>
2017-05-01 09:25:31 -0400

    Update ingest profiles api

--- ingest-profiles-api/v1/src/profiles.js
+++ ingest-profiles-api/v1/src/profiles.js
@@ -40,7 +40,7 @@
  * @apiParam (Request Body Fields) {Number} [renditions.keyframe_rate] Maximum number of keyframes per second
  * @apiParam (Request Body Fields) {String="poster","thumbnail"} [renditions.label] Image type for image renditions; __required__ for image renditions
  * @apiParam (Request Body Fields) {Boolean} [renditions.live_stream] Whether this will be for live streaming video
- * @apiParam (Request Body Fields) {Number{300-1800}} [renditions.live_sliding_window_duration] Duration of stream to keep available for LiveDVR delivery (in seconds)
+ * @apiParam (Request Body Fields) {Number{300-1800}} [renditions.live_sliding_window_duration=300] Duration of stream to keep available for LiveDVR delivery (in seconds)
  * @apiParam (Request Body Fields) {Number} [renditions.max_video_bitrate] Maximum video bitrate (h.264 only)
  * @apiParam (Request Body Fields) {Number} [renditions.max_frame_rate] Limits the frame rate rather than sets it, use as an alternative to frame rate
  * @apiParam (Request Body Fields) {String="video","audio","image"} [renditions.media_type="video"] the media type of the rendition
@@ -641,7 +641,7 @@
  * @apiParam (Request Body Fields) {Number} [renditions.keyframe_rate] Maximum number of keyframes per second
  * @apiParam (Request Body Fields) {String="poster","thumbnail"} [renditions.label] Image type for image renditions; __required__ for image renditions
  * @apiParam (Request Body Fields) {Boolean} [renditions.live_stream] Whether this will be for live streaming video
- * @apiParam (Request Body Fields) {Number{300-1800}} [renditions.live_sliding_window_duration] Duration of stream to keep available for LiveDVR delivery (in seconds)
+ * @apiParam (Request Body Fields) {Number{300-1800}} [renditions.live_sliding_window_duration=300] Duration of stream to keep available for LiveDVR delivery (in seconds)
  * @apiParam (Request Body Fields) {Number} [renditions.max_video_bitrate] Maximum video bitrate (h.264 only)
  * @apiParam (Request Body Fields) {Number} [renditions.max_frame_rate] Limits the frame rate rather than sets it, use as an alternative to frame rate
  * @apiParam (Request Body Fields) {String="video","audio","image"} [renditions.media_type="video"] the media type of the rendition
