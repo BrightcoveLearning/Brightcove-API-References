5a2fcecdc0a6069db23d56aa4685b74a38a0b81e
Jeff Doktor <jdoktor@brightcove.com>
2017-06-07 12:29:32 -0600

    new format

--- playback-api/v1/src/video.js
+++ playback-api/v1/src/video.js
@@ -521,12 +521,12 @@
  *
  * @apiError (Error 400) BAD_REQUEST error_subcode: `DUPLICATE_PARAMETERS` - The same parameter name was provided more than once in the request
  * @apiError (Error 401) ACCESS_DENIED Must legal policy key in an [appropriate header](http://docs.brightcove.com/en/video-cloud/playback-api/getting-started/api-overview.html#authentication).
- * @apiError (Error 403) ACCOUNT_ID FORBIDDEN - The account id in the policy key does not match the account in the api request
- * @apiError (Error 403) DOMAIN FORBIDDEN - The video is restricted from playing on the current domain
- * @apiError (Error 403) CLIENT_GEO FORBIDDEN - The video is restricted from playing in the current geo region; the message will contain additional information about the specific issue. For more details, see the [Playback API Error Reference](http://docs.brightcove.com/en/video-cloud/playback-api/references/error-reference.html)
- * @apiError (Error 403) CLIENT_IP FORBIDDEN - The video is restricted at the current IP address
- * @apiError (Error 403) POLICY_ERROR FORBIDDEN - Error when evaluating the policy key
- * @apiError (Error 403) VIDEO_NOT_PLAYABLE FORBIDDEN - For a single video request, the video exists, but is not allowed to be played now. That could be any of the three reasons that videos are not playable: not sufficiently ingested, not active, not in scheduled date range.
+ * @apiError (Error 403) FORBIDDEN(1) error_subcode: `ACCOUNT_ID`  - The account id in the policy key does not match the account in the api request
+ * @apiError (Error 403) FORBIDDEN(2) error_subcode: `DOMAIN` - The video is restricted from playing on the current domain
+ * @apiError (Error 403) FORBIDDEN(3) error_subcode: `CLIENT_GEO` - The video is restricted from playing in the current geo region; the message will contain additional information about the specific issue. For more details, see the [Playback API Error Reference](http://docs.brightcove.com/en/video-cloud/playback-api/references/error-reference.html)
+ * @apiError (Error 403) FORBIDDEN(4) error_subcode: `CLIENT_IP` - The video is restricted at the current IP address
+ * @apiError (Error 403) FORBIDDEN(5) error_subcode: `POLICY_ERROR` - Error when evaluating the policy key
+ * @apiError (Error 403) FORBIDDEN(6) error_subcode: `VIDEO_NOT_PLAYABLE` - For a single video request, the video exists, but is not allowed to be played now. That could be any of the three reasons that videos are not playable: not sufficiently ingested, not active, not in scheduled date range.
  * @apiError (Error 404) NOT_FOUND error_subcode: `VIDEO_NOT_FOUND` - The requested resource is not available.
  * @apiError (Error 405) METHOD_NOT_ALLOWED Only `GET`, `HEAD` and `OPTIONS` are allowed for this api.
  * @apiError (Error 500) SERVER_ERROR Internal server error.
