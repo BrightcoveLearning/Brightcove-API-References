3998efbe83b70928f9f8b209015e19e966130823
Jeff Doktor <jdoktor@brightcove.com>
2017-06-07 10:23:07 -0600

    update video error codes for PAPI

--- playback-api/v1/src/video.js
+++ playback-api/v1/src/video.js
@@ -519,20 +519,21 @@
 * "created_at": "2017-06-05T11:20:52.412Z"
 *}
  *
- * @apiError (Error 400) BAD_REQUEST Mis-formatted request, i.e. duplicate query parameters supplied
- * @apiError (Error 401) INVALID_POLICY_KEY Must provide a BCOV-Policy header with a legal policy key
- * @apiError (Error 401) FORBIDDEN Video cannot be played back at this location because of the domain, ip, or geo restriction policy
- * @apiError (Error 404) VIDEO_NOT_FOUND The designated resource was not found
- * @apiError (Error 404) RESOURCE_NOT_FOUND The designated resource was not found
- * @apiError (Error 404) ACCOUNT_NOT_FOUND The designated resource was not found
- * @apiError (Error 405) METHOD_NOT_ALLOWED Only GET, HEAD and OPTIONS are allowed for this api
- * @apiError (Error 500) INTERNAL_SERVER_ERROR Something went wrong on our side while handling the request
- * @apiError (Error 502) BAD_GATEWAY Bad response from a backend server
- * @apiError (Error 502) ACCOUNT_RETRIEVE_FAILURE Bad response from a backend server
- * @apiError (Error 502) VIDEO_RETRIEVE_FAILURE Bad response from a backend server
- * @apiError (Error 502) VIDEO_URLS_RETRIEVE_FAILURE Bad response from a backend server
- * @apiError (Error 503) SERVICE_UNAVAILABLE Returned this response from a backend server
- * @apiError (Error 504) GATEWAY_TIMEOUT Either a backend server or one of the servers they rely on timed out
+ * @apiError (Error 400) BAD_REQUEST error_subcode: `DUPLICATE_PARAMETERS` - The same parameter name was provided more than once in the request
+ * @apiError (Error 401) ACCESS_DENIED Must legal policy key in an [appropriate header](http://docs.brightcove.com/en/video-cloud/playback-api/getting-started/api-overview.html#authentication).
+ * @apiError (Error 403) FORBIDDEN error_subcode: `ACCOUNT_ID` - The account id in the policy key does not match the account in the api request
+ * @apiError (Error 403) FORBIDDEN error_subcode: `DOMAIN` - The video is restricted from playing on the current domain
+ * @apiError (Error 403) FORBIDDEN error_subcode: `CLIENT_GEO` - The video is restricted from playing in the current geo region; the message will contain additional information about the specific issue. For more details, see the [Playback API Error Reference](http://docs.brightcove.com/en/video-cloud/playback-api/references/error-reference.html)
+ * @apiError (Error 403) FORBIDDEN error_subcode: `CLIENT_IP` - The video is restricted at the current IP address
+ * @apiError (Error 403) FORBIDDEN error_subcode: `POLICY_ERROR` - Error when evaluating the policy key
+ * @apiError (Error 403) FORBIDDEN error_subcode: `VIDEO_NOT_PLAYABLE` - For a single video request, the video exists, but is not allowed to be played now. That could be any of the three reasons that videos are not playable: not sufficiently ingested, not active, not in scheduled date range.
+ * @apiError (Error 404) NOT_FOUND error_subcode: `VIDEO_NOT_FOUND` - The requested resource is not available.
+ * @apiError (Error 405) METHOD_NOT_ALLOWED Only `GET`, `HEAD` and `OPTIONS` are allowed for this api.
+ * @apiError (Error 500) SERVER_ERROR Internal server error.
+ * @apiError (Error 502) SERVER_ERROR Got a bad response from a backend server. Various *_RETRIEVE_FAILURE error codes: `ACCOUNT_RETRIEVE_FAILURE`, `VIDEO_RETRIEVE_FAILURE`, `VIDEO_URLS_RETRIEVE_FAILURE`, `PLAYLIST_RETRIEVE_FAILURE`, `PLAYLIST_VIDEOS_RETRIEVE_FAILURE`.
+ * @apiError (Error 503) SERVICE_UNAVAILABLE Returned this response from a backend server.
+ * @apiError (Error 504) SERVER_TIMEOUT Either a backend server or one of the servers they rely on timed out.
+.
  *
  * @apiErrorExample {json} 404 Error Response
  *     HTTP/1.1 404 Not Found
