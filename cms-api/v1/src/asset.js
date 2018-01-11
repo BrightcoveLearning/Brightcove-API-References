121a505ca69c4d3c0ebe1ddcc8857dd2ad331586
Robert Crooks <rcrooks@brightcove.com>
2018-01-11 10:45:36 -0500

    fix typo cms api ref

--- cms-api/v1/src/asset.js
+++ cms-api/v1/src/asset.js
@@ -456,7 +456,7 @@
  * @apiSuccess (Response Fields) {DateString} updated_at when the video was last modified
  * @apiSuccess (Response Fields) {DateString} uploaded_at when the asset was added to the video in Video Cloud
  * @apiParam (Request Body Fields) {String} video_codec the video codec &mdash; support for video formats -  varies across browsers and devices; for the broadest support use `h264` &mdash; __remote assets will not work properly without this field__
- * @apiParam (Request Body Fields) {String} video_container the video container such as `MP4` or `MT2S` &mdash; __remote assets will not work properly without this field__
+ * @apiParam (Request Body Fields) {String} video_container the video container such as `MP4` or `M2TS` &mdash; __remote assets will not work properly without this field__
  * @apiSuccess (Response Fields) {Number} video_duration duration in milliseconds
  *
  * @apiSuccessExample {json} Success Response:
@@ -590,7 +590,7 @@
  * @apiParam (Request Body Fields) {Number} [size] the size of the asset in bytes (integer)
  * @apiParam (Request Body Fields) {Number} [video_duration] duration in milliseconds
  * @apiParam (Request Body Fields) {String} [video_codec] the video codec &mdash; support for video formats varies across browsers and devices; for the broadest support use `h264`
- * @apiParam (Request Body Fields) {String} [video_container] the video container such as `MP4` or `MT2S`
+ * @apiParam (Request Body Fields) {String} [video_container] the video container such as `MP4` or `M2TS`
  *
  * @apiParamExample {json} Add Rendition Request Body Example:
  *    {
