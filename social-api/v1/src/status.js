78e760aab3f01fd8afb0cd18ec2d9db2bf9260bf
Robert Crooks <rcrooks@brightcove.com>
2018-03-01 17:07:32 -0500

    wip

--- social-api/v1/src/status.js
+++ social-api/v1/src/status.js
@@ -1,44 +1,23 @@
-// create policy
+// Get Status of Video
 
 /**
- * @api {post} /accounts/:account_id/policy_keys Create a Policy Key
- * @apiName Create a Policy Key
- * @apiGroup Policy
+ * @api {get} /accounts/:account_id/social-status/video/:video_id Get Status of Video
+ * @apiName Get Status of Video
+ * @apiGroup Status
  * @apiVersion 1.0.0
  *
- * @apiDescription Create a new policy key to access the Playback API
+ * @apiDescription Get the current status of the requested video on every destination to which Social has ever attempted to distribute it, ordered by date, most recent first.
  *
  * @apiHeader {String} Content-Type Content-Type: application/json
  * @apiHeader {String} Authorization Authorization: Bearer access_token (see [Getting Access Tokens](https://support.brightcove.com/getting-access-tokens))
  *
  * @apiParam (Path Parameters) {String} account_id Video Cloud account ID
- * @apiParam (Request Body Fields) {Object} key-data Data for the policy key
- * @apiParam (Request Body Fields) {String} key-data.account-id Video Cloud account id
- * @apiParam (Request Body Fields) {String[]} [key-data.apis] Array of APIs that are permitted for this key (currently `"search"` is the only one available - this must be included to use the [search functionality](https://brightcovelearning.github.io/Brightcove-API-References/playback-api/v1/doc/index.html#api-videoGroup-Get_Videos) for the Playback API)
- * @apiParam (Request Body Fields) {String[]} [key-data.allowed-domains] For domain restriction, the domains this key will work on
- * @apiParam (Request Body Fields) {Boolean} [key-data.require-ad-config=false] Whether Playback API requests require an `ad-config-id` URL parameter for server-side ad insertion
- * @apiParam (Request Body Fields) {Object} [key-data.geo={}] Map of geo-filtering properties
- * @apiParam (Request Body Fields) {String[]} [key-data.geo.countries=null] Array of [ISO 3166 list of 2- or 4-letter codes __in lower-case__](https://www.iso.org/obp/ui/#home) (search for "country codes")
- * @apiParam (Request Body Fields) {Boolean} [key-data.geo.exclude_countries=false] If true, country array is treated as a list of countries excluded from viewing. If false, the country array is a list of countries included for viewing.
+ * @apiParam (Path Parameters) {String} video_id Video ID
+ * @apiParam (URL Parameters) {DateTime} [before] Only videos last modified at or before the specified timestamp will be returned - ISO 8601 Timestamp
+ * @apiParam (URL Parameters) {DateTime} [since] Only videos last modified at or since the specified timestamp will be returned - ISO 8601 Timestamp
  *
- * @apiParamExample {json} Create Policy Request Body Examples:
- * {
- * "key-data": {
- *    "account-id": "123456789000",
- *    "require-ad-config": true,
- *    "apis": [
- *      "search"
- *    ],
- *    "allowed-domains": [
- *      "http://support.brightcove.com",
- *      "https://solutions.brightcove.com"
- *    ],
- *    "geo": {
- *      "countries": ["us", "usmil", "pr", "gu", "vi", "as", "mp"],
- *      "exclude_countries": false
- *    }
- *  }
- *}
+ * @apiParamExample {http} Get Status of Video Examples:
+ *
  *
  * @apiSuccess (Response Fields) {String} key_string The policy key string
  * @apiSuccess (Response Fields) {Object} key-data Map of key data prescribing the policy
