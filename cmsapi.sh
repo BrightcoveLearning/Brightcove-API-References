9cd0d3b6bd266e5f9ff5a5e9363fa5328505913b
Robert Crooks <rcrooks@brightcove.com>
2018-04-25 07:22:28 -0400

    wip

--- cmsapi.sh
+++ cmsapi.sh
@@ -7,6 +7,7 @@ echo ${txtyel}generating docs for cms-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-v3
 apidoc -i v1/src/  -f .js -o v1/drupal/ -t ../template-d
 echo ${txtyel}generating swagger json
+cd ..
 apidoc-swagger -i ./cms-api -o ./cms-api/swagger
 echo ${txtgrn}swagger json generated
 echo ${txtgrn}Finished!
