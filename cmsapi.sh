dff8ab0c40772440f2903269be27581ebdbd3e44
Robert Crooks <rcrooks@brightcove.com>
2018-04-25 09:19:07 -0400

    comment out something we don't need for now

--- cmsapi.sh
+++ cmsapi.sh
@@ -5,7 +5,7 @@ txtred=$(tput setaf 9) # red
 cd cms-api
 echo ${txtyel}generating docs for cms-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-v3
-apidoc -i v1/src/  -f .js -o v1/drupal/ -t ../template-d
+# apidoc -i v1/src/  -f .js -o v1/drupal/ -t ../template-d
 echo ${txtyel}generating swagger json
 apidoc-swagger -i ../cms-api -o ../cms-api/swagger
 echo ${txtgrn}swagger json generated
