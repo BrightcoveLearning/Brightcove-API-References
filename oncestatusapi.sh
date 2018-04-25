61d2db746eeb838dce7620406a9a4b82fbe30df3
Robert Crooks <rcrooks@brightcove.com>
2018-04-25 09:11:16 -0400

    update script

--- oncestatusapi.sh
+++ oncestatusapi.sh
@@ -5,5 +5,8 @@ txtred=$(tput setaf 9) # red
 cd once-status-api
 echo ${txtyel}generating docs for once-status-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-v3
+echo ${txtyel}generating swagger json
+apidoc-swagger -i ../once-status-api/ -o ../once-status-api/swagger
+echo ${txtgrn}swagger json generated
 echo ${txtgrn}Finished!
 echo ${txtrst}
