6bf5ea99cb4ccd4582013d822d9c9781d7676c39
Robert Crooks <rcrooks@brightcove.com>
2018-04-25 08:17:21 -0400

    update script

--- liveapi.sh
+++ liveapi.sh
@@ -5,5 +5,8 @@ txtred=$(tput setaf 9) # red
 cd live-api
 echo ${txtyel}generating docs for live-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-v3
+echo ${txtyel}generating swagger json
+apidoc-swagger -i ../live-api/ -o ../live-api/swagger
+echo ${txtgrn}swagger json generated
 echo ${txtgrn}Finished!
 echo ${txtrst}
