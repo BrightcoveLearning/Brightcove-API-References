24469e13c665e739a52e13269a1ed364cb7d338f
rcrooks <rcrooks@brightcove.com>
2018-04-25 14:31:49 -0400

    wip

--- cms-api/v1/src/video.js
+++ cms-api/v1/src/video.js
@@ -35,13 +35,13 @@
  * @apiSuccess (Response Fields) {String} created_at when the video was created
  * @apiSuccess (Response Fields) {String} ad_keys string representing the ad key/value pairs assigned to the video. Key/value pairs are formatted as key=value and are separated by ampersands. For example: `"adKeys": "category=sports&live=true"`
  * @apiSuccess (Response Fields) {String} clip_source_video_id The ID of the source video that was clipped to produce this video or `null` if this video is not a clip of another video
- * @apiSuccess (Response Fields) {Object} custom_fields={} map of fieldname-value pairs
+ * @apiSuccess (Response Fields) {Object} custom_fields map of fieldname-value pairs
  * @apiSuccess (Response Fields) {Object} cue_points array of cue point maps
  * @apiSuccess (Response Fields) {String} cue_points.name cue point name
  * @apiSuccess (Response Fields) {String} cue_points.type cue point type
  * @apiSuccess (Response Fields) {Number} cue_points.time time of the cue point in seconds; example: 10.527
- * @apiSuccess (Response Fields) {String} cue_points.metadata=null optional metadata string (128 single-byte characters maximum)
- * @apiSuccess (Response Fields) {Boolean} cue_points.force_stop=false whether video is force_stopped at the cue point
+ * @apiSuccess (Response Fields) {String} cue_points.metadata optional metadata string (128 single-byte characters maximum)
+ * @apiSuccess (Response Fields) {Boolean} cue_points.force_stop whether video is force_stopped at the cue point
  * @apiSuccess (Response Fields) {String} delivery_type video delivery type - `remote`, `static_origin`, `dynamic_origin` or `unknown`
  * @apiSuccess (Response Fields) {String} description video short description
  * @apiSuccess (Response Fields) {Boolean} drm_disabled if `true`, the video is not DRM-packaged
@@ -50,10 +50,10 @@
  * @apiSuccess (Response Fields) {String} economics whether video is AD_SUPPORTED
  * @apiSuccess (Response Fields) {String} folder_id id for the folder the video belongs to
  * @apiSuccess (Response Fields) {Object} geo map of geo-filtering properties
- * @apiSuccess (Response Fields) {String} geo.countries=null array of ISO 3166 list of 2-letter codes(https://www.iso.org/obp/ui/#home) (search for "country codes")
- * @apiSuccess (Response Fields) {Boolean} geo.exclude_countries=false if true, country array is treated as a list of countries excluded from viewing
- * @apiSuccess (Response Fields) {Boolean} geo.restricted=false whether geo-restriction is enabled for this video
- * @apiSuccess (Response Fields) {Boolean} has_digital_master=false whether video has an archived master than can be used for retranscoding
+ * @apiSuccess (Response Fields) {String} geo.countries array of ISO 3166 list of 2-letter codes(https://www.iso.org/obp/ui/#home) (search for "country codes")
+ * @apiSuccess (Response Fields) {Boolean} geo.exclude_countries if true, country array is treated as a list of countries excluded from viewing
+ * @apiSuccess (Response Fields) {Boolean} geo.restricted whether geo-restriction is enabled for this video
+ * @apiSuccess (Response Fields) {Boolean} has_digital_master whether video has an archived master than can be used for retranscoding
  * @apiSuccess (Response Fields) {Object} images map of image maps
  * @apiSuccess (Response Fields) {Object} images.poster map of poster properties
  * @apiSuccess (Response Fields) {String} images.poster.asset_id asset id for the poster
