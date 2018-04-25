46de02b0b553d4188251e8fd58667546f20e1d70
Robert Crooks <rcrooks@brightcove.com>
2018-04-25 07:59:53 -0400

    fix

--- audienceapi.sh
+++ audienceapi.sh
@@ -6,7 +6,7 @@ cd audience-api
 echo ${txtyel}generating docs for audience-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-v3
 echo ${txtyel}generating swagger json
-apidoc-swagger -i audinence-api/ -o audinence-api/swagger
+apidoc-swagger -i ../audience-api/ -o ../audience-api/swagger
 echo ${txtgrn}swagger json generated
 echo ${txtgrn}Finished!
 echo ${txtrst}
