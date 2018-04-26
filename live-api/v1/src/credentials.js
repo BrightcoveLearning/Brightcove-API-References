661c6dc07964d9e1ce74474f09d776a89ab67efd
rcrooks <rcrooks@brightcove.com>
2018-04-26 15:36:29 -0400

    wip

--- live-api/v1/src/credentials.js
+++ live-api/v1/src/credentials.js
@@ -12,7 +12,7 @@
   * @apiHeader {String} X-API-KEY X-API-KEY: {APIKey}
   *
   *
-  * @apiParamExample {http} List Credentials Example:
+  * @apiParamExample {String} List Credentials Example:
   *    https://api.bcovlive.io/v1/credentials
   *
   *
@@ -21,17 +21,72 @@
   * @apiSuccess (200) {String} vod_jobs.label The clip label (from the input)
   * @apiSuccess (200) {String} live_job_id The clip label (from the input)
   *
-  * @apiSuccessExample {json} Creation of a clip
-  *    {
-  *      "vod_jobs": [
-  *        {
-  *          "jvod_id": "9582606c50d84be5ad4bc104f2aa3360",
-  *          "label": "last 60 secs of live job"
-  *        }
-  *      ],
-  *      "live_job_id": "88ba5d87b61a4ef3a6dddabd0c38d319"
-  *    }
-  *
+  * @apiSuccessExample {Object} Creation of a clip
+  {
+  	"user_id": "c2691d4d039040be96c190a949d754a7",
+  	"credentials": [
+  		{
+  			"credential_default_for_type": true,
+  			"user_id": "c2691d4d039040be96c190a949d754a7",
+  			"credential_private": "*************",
+  			"credential_type": "videocloud",
+  			"credential_label": "bc-rcrooks-vc",
+  			"credential_id": "cbdd669730aa4ecfa34a74913df32026",
+  			"credential_public": "553d4903-4547-435d-944c-2c8e2f6abc5d"
+  		},
+  		{
+  			"credential_default_for_type": true,
+  			"user_id": "c2691d4d039040be96c190a949d754a7",
+  			"credential_type": "oauth",
+  			"credential_label": "bcls-oauth",
+  			"credential_private": "************ey",
+  			"oauth_settings": {
+  				"url": "https://api.bcovlive.io/oauth/authorize"
+  			},
+  			"credential_id": "ac0f35e240b24081b38ba3e5f040536e",
+  			"credential_public": "secret-client"
+  		},
+  		{
+  			"credential_default_for_type": false,
+  			"user_id": "c2691d4d039040be96c190a949d754a7",
+  			"credential_type": "oauth",
+  			"credential_label": "rcrooks-oauth",
+  			"credential_private": "************ey",
+  			"oauth_settings": {
+  				"url": "https://api-mocks.a-live.io/oauth/authorize"
+  			},
+  			"credential_id": "85bc309ce70049b2b0659e4c0ef85432",
+  			"credential_public": "secret-client"
+  		},
+  		{
+  			"credential_default_for_type": false,
+  			"user_id": "c2691d4d039040be96c190a949d754a7",
+  			"credential_type": "videocloud",
+  			"credential_label": "BCLearning",
+  			"credential_private": "************G6ABYS1FMDVE4JNDQ",
+  			"credential_id": "38385b978469409482e5d59af423b309",
+  			"credential_public": "b10631d3-7597-4be8-b8b5-dce142f81006"
+  		},
+  		{
+  			"credential_default_for_type": false,
+  			"user_id": "c2691d4d039040be96c190a949d754a7",
+  			"credential_private": "************9KeXmce9",
+  			"credential_type": "s3",
+  			"credential_label": "BCLS-S3",
+  			"credential_id": "646ae46351074fcdaea52542903c3d16",
+  			"credential_public": "AKIAIVCKQ7XZRGUIBBWQ"
+  		},
+  		{
+  			"credential_default_for_type": true,
+  			"user_id": "c2691d4d039040be96c190a949d754a7",
+  			"credential_private": "************240aef",
+  			"credential_type": "zencoder",
+  			"credential_label": "alive-zencoder",
+  			"credential_id": "bc303a92793249dcb759a114dbbfe7c2",
+  			"credential_public": "Credentials for Zencoder"
+  		}
+  	]
+  }  *
   * @apiError (Error 4xx) {json} BAD REQUEST 400: Invalid input value - see [Live API Error Messages](https://support.brightcove.com/live-api-error-messages) for more details
   * @apiError (Error 4xx) {json} BAD REQUEST 400: The notification target type is not supported currently - see [Live API Error Messages](https://support.brightcove.com/live-api-error-messages) for more details
   * @apiError (Error 4xx) {json} UNAUTHORIZED 401: Unauthorized - see [Live API Error Messages](https://support.brightcove.com/live-api-error-messages) for more details
@@ -41,23 +96,23 @@
   *
   */
 
-// List Credentials
+// Create OAuth Credential
 
 /**
-  * @api {get} /v1/credentials List Credentials
-  * @apiName List Credentials
+  * @api {post} /v1/credentials Create OAuth Credential
+  * @apiName Create OAuth Credential
   * @apiGroup Credentials
   * @apiVersion 1.0.0
   *
-  * @apiDescription Get a list of credentials for a Live account.
+  * @apiDescription Create an OAuth credential for notifications.
   *
   * @apiHeader {String} Content-Type Content-Type: application/json
   * @apiHeader {String} X-API-KEY X-API-KEY: {APIKey}
   *
-  * @apiParam (Request Body Fields) {String} live_job_id The id of Live Stream job to create the VOD clip from.
-  * @apiParam (Request Body Fields) {Object[]} outputs Array of VOD outputs
-  * @apiParam (Request Body Fields) {String} outputs.label Label for the output
-  * @apiParam (Request Body Fields) {Number} outputs.duration Duration of the clip in seconds
+  * @apiParam (Request Body Fields) {String} user_id The user id, which you can get from the List Credentials response.
+  * @apiParam (Request Body Fields) {Boolean} credential_default_for_type Whether these are the default credentials for the request type
+  * @apiParam (Request Body Fields) {String} credential_label Label for the credential
+  * @apiParam (Request Body Fields) {Number} credential_type Duration of the clip in seconds
   * @apiParam (Request Body Fields) {Number} outputs.stream_start_time Start time in seconds for the clip relative to the start time of the live stream
   * @apiParam (Request Body Fields) {Number} outputs.stream_end_time End time in seconds for the clip relative to the start time of the live stream
   * @apiParam (Request Body Fields) {Number} outputs.stream_start_timecode Start for the clip as an SMPTE timecode for the live stream
