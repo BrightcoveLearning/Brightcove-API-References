956327adc824c2acbc7feb900994361740240ca5
rcrooks <rcrooks@brightcove.com>
2018-05-01 14:55:25 -0400

    remove swagger gen

--- oauthapi.sh
+++ oauthapi.sh
@@ -5,8 +5,5 @@ txtred=$(tput setaf 9) # red
 cd oauth-api
 echo ${txtyel}generating docs for oauth-api
 apidoc -i v4/src/  -f .js -o v4/doc/ -t ../template-v3
-echo ${txtyel}generating swagger json
-apidoc-swagger -i ../oauth-api/ -o ../oauth-api/swagger
-echo ${txtgrn}swagger json generated
 echo ${txtgrn}Finished!
 echo ${txtrst}
