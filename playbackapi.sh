94444f093fa1195742443fb9979c2cadb2b3c911
rcrooks <rcrooks@brightcove.com>
2018-05-01 14:56:39 -0400

    remove swagger gen

--- playbackapi.sh
+++ playbackapi.sh
@@ -5,8 +5,5 @@ txtred=$(tput setaf 9) # red
 cd playback-api
 echo ${txtyel}generating docs for playback-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-v3
-echo ${txtyel}generating swagger json
-apidoc-swagger -i ../playback-api/ -o ../playback-api/swagger
-echo ${txtgrn}swagger json generated
 echo ${txtgrn}Finished!
 echo ${txtrst}
