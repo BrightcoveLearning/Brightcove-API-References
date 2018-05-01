bb3cde7c87dd8e3c3f8d21683dc6f684f60ddd3a
rcrooks <rcrooks@brightcove.com>
2018-05-01 14:53:01 -0400

    remove swagger gen

--- audienceapi.sh
+++ audienceapi.sh
@@ -5,8 +5,5 @@ txtred=$(tput setaf 9) # red
 cd audience-api
 echo ${txtyel}generating docs for audience-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-v3
-echo ${txtyel}generating swagger json
-apidoc-swagger -i ../audience-api/ -o ../audience-api/swagger
-echo ${txtgrn}swagger json generated
 echo ${txtgrn}Finished!
 echo ${txtrst}
