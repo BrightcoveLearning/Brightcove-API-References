e4c71cf43b561fecc9c8baa3071beb0a93461263
rcrooks <rcrooks@brightcove.com>
2018-05-01 14:53:52 -0400

    remove swagger gen

--- diapi.sh
+++ diapi.sh
@@ -5,8 +5,5 @@ txtred=$(tput setaf 9) # red
 cd dynamic-ingest-api
 echo ${txtyel}generating docs for dynamic-ingest-api
 apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-v3
-echo ${txtyel}generating swagger json
-apidoc-swagger -i ../dynamic-ingest-api/ -o ../dynamic-ingest-api/swagger
-echo ${txtgrn}swagger json generated
 echo ${txtgrn}Finished!
 echo ${txtrst}
