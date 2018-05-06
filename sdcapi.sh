a395ba3cea7277cca691f93756af148cd0bf9380
Robert Crooks <rcrooks@brightcove.com>
2018-05-06 13:24:24 -0400

    update path

--- sdcapi.sh
+++ sdcapi.sh
@@ -5,7 +5,7 @@ txtgrn=$(tput setaf 6) # Green
 txtred=$(tput setaf 9) # red
 cd data-collection-api
 echo ${txtyel}generating swagger json
-node $SWAGGER_DIR/bin/apidocSwagger.js -i ./v1/src -o ./swagger -t
+node $SWAGGER_DIR/bin/apidocSwagger.js -i ./v2/src -o ./swagger -t
 echo ${txtgrn}swagger json generated
 echo ${txtgrn}Finished!
 echo ${txtrst}
