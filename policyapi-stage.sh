70b6d2c0939dba4fad258ffb7ef027ca490c5bed
Robert Crooks <rcrooks@brightcove.com>
2018-10-22 11:54:43 -0400

    more

--- policyapi-stage.sh
+++
@@ -1,9 +0,0 @@
-txtrst=$(tput sgr0) # Text reset
-txtyel=$(tput setaf 7) # Yellow
-txtgrn=$(tput setaf 6) # Green
-txtred=$(tput setaf 9) # red
-cd policy-api-stage
-echo ${txtyel}generating docs for policy-api-stage
-apidoc -i v1/src/  -f .js -o v1/doc/ -t ../template-v3
-echo ${txtgrn}Finished!
-echo ${txtrst}
